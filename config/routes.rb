93d227e804e68b34d4a666224bbe494e22be7b4b
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-04-19 16:31:53 +0900

    first_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'messages#index'
+  get 'messages' => 'messages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
