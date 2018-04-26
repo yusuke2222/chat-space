40fdbd2e013de3e23a0dd1786717a37652b5c6aa
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-04-26 15:56:14 +0900

    rooting configuration

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
