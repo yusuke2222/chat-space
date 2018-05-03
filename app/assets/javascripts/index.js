5901eb9024b1b78f9fcec59e6c1fc1eac0b13596
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-05-03 14:48:27 +0900

    Add_Remove_User

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -38,4 +38,23 @@ $(function() {
       alert('ユーザー検索に失敗しました');
     });
   });
+  function appendUserRemove(user_id, user_name) {
+      var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${ user_id }'>
+                    <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
+                    <p class='chat-group-user__name'>${ user_name }</p>
+                    <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                  </div>`
+      $("#chat-group-users").append(html);
+  };
+
+  $('#user-search-result').on('click', '.chat-group-user__btn--add', function(){
+    var user_id = $(this).attr('data-user-id');
+    var user_name = $(this).attr('data-user-name');
+  $(this).parent().remove();
+    appendUserRemove(user_id, user_name);
+  });
+
+  $('#chat-group-users').on('click', '.chat-group-user__btn--remove', function(){
+    $(this).parent().remove();
+  });
 });
