eae0afc1c67d5f7aae9644db58e83c25692da2f4
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-05-03 14:25:46 +0900

    Make js-file

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,41 @@
+$(function() {
+
+  function appendUser(user){
+  console.log(user)
+  var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+              </div>`
+  $("#user-search-result").append(html);
+  }
+
+  function appendNoUser(user) {
+    var html = `<div class="chat-group-user clearfix">${user}</div>`
+    $("#user-search-result").append(html);
+  }
+
+  $(".chat-group-form__input").on("keyup", function() {
+    var input = $("#user-search-field").val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+    .done(function(users) {
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+      });
+      }
+      else {
+        appendNoUser("一致するユーザーはいません");
+      }
+    })
+    .fail(function() {
+      alert('ユーザー検索に失敗しました');
+    });
+  });
+});
